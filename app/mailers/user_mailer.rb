88f6c6a100e5f573b8f2b9a3fa0e2f7a773b1b0c
benbrabston <bbrabsto@chicagobooth.edu>
2015-04-16 21:42:40 +0000

    Got em

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,6 @@ class UserMailer < ApplicationMailer
         @request = request
         @requestee = request.requestee
         @requestor = request.requestor
-        @feeback_link = url_for(:controller => 'requests', :id => request.id)
         mail(to: @requestee.email, subject: "You have a new feedback request from #{@requestor.first_last}")
     end
 end
